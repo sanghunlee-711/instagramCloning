7d6e41eb50b9e262c8f5fdf8aaf8e48085fcbfd7
sanghunLee <cloudlee711@gmail.com>
2020-07-29 21:27:58 +0900

    add button color change

---
+++ js/login.js
@@ -0,0 +1,56 @@
+
+const idinput = document.getElementById("wrapperRightContentInputId"); 
+const pw = document.getElementById("wrapperRightContentInputPw");
+
+const btnColorChangeid = () => {
+    const id = document.getElementById("wrapperRightContentInputId").value;
+    const pw = document.getElementById("wrapperRightContentInputPw").value;
+    const btn = document.getElementById("LoginButton"); 
+    if(id.length>=1 && pw.length>=1){
+        btn.style.backgroundColor = "blue";
+    }
+    else{
+        btn.style.backgroundColor = "#C0DFFD";
+    }
+}
+
+const btnColorChangepw = () => {
+    const id = document.getElementById("wrapperRightContentInputId").value;
+    const pw = document.getElementById("wrapperRightContentInputPw").value;
+    const btn = document.getElementById("LoginButton"); 
+    if(pw.length >=1 && id.length >= 1 ){
+        btn.style.backgroundColor = "blue";
+    }
+    else{
+        btn.style.backgroundColor = "#C0DFFD";
+    }
+}
+
+btnColorChangeid();
+btnColorChangepw();
+
+idinput.addEventListener("keyup",btnColorChangeid);
+pw.addEventListener("keyup",btnColorChangepw);
+
+
+
+
+
+/*
+const btncolorchange = ()=>{
+    const id = document.querySelector("#wrapperRightContentInputId").value;
+    const pw = document.querySelector("#wrapperRightContentInputPw").value;
+    const btn = document.querySelector("#LoginButton");
+
+if (id == pw){
+    result = btn.style.backgroundColor = "blue";
+}
+else{
+    result = btn.style.backgroundColor = "white";
+}
+    return result;
+}
+
+btncolorchange();
+
+*/
